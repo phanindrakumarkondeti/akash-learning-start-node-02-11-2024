da2061d019d7c9e527bd2a7c6718f1d2603b346e
phanindrakumarkondeti <phani23phani@gmail.com>
2024-11-11 16:49:06 +0530

    morgan and ejs day 5

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
+let fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config()
+let morgan = require('morgan');
+
 const port = process.env.PORT || 6700; // Use an environment variable if available
 let categoryRouter = require('./src/controller/CategoryRouter');
 let productRouter = require('./src/controller/ProductRouter');
 
 //let port = 6700;
-
-
+app.use(morgan('common',{stream:fs.createWriteStream("./app.log")}));
+//app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname+'/public'));
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
 //default
 app.get('/', (req, res) => {
-    res.send('Welcome to the express server')
+   // res.send('Welcome to the express server')
+   res.render('index',{title: 'Welcome to the express server'});
 });
 
 
